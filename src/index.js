0a4cf3db7ee9d83ac35a2b42554ef107a2d98c5e
Sultanaladawi <sultanadawi2004@gmail.com>
2026-07-04 12:56:59 +0300

    feat: Configure Progressive Web App (PWA) with service worker sw.js and theme color adjustments

--- src/index.js
+++ src/index.js
@@ -9,3 +9,11 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(reg => console.log('Service Worker registered'))
+      .catch(err => console.error('Service Worker registration failed', err));
+  });
+}
